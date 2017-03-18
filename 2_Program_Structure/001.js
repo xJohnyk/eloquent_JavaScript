cee6c623d205bcc747f22406723900e2608e91d6
xJohnyk <wojciech.skrzek@gmail.com>
2017-03-18 14:20:18 +0100

    Add ChessBoard

---
+++ 2_Program_Structure/001.js
@@ -0,0 +1,15 @@
+//Write a loop that makes seven calls to console.log to output the following
+//triangle:
+//#
+//##
+//###
+//####
+//#####
+//######
+//#######
+
+var k = "#"
+for(var i =0;i<6;i++){
+    console.log(k);
+    k = k + "#";
+}
