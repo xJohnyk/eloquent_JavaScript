f231115deea58fa73492b0651d4542306b6b91e2
xJohnyk <wojciech.skrzek@gmail.com>
2017-03-19 19:28:39 +0100

    Folder 003 Functions and JS script 004

---
+++ 3_Functions/004_Minimum.js
@@ -0,0 +1,11 @@
+//The previous chapter introduced the standard function Math.min that returns
+//its smallest argument. We can do that ourselves now. Write a
+//function min that takes two arguments and returns their minimum
+
+function mini(a,b){
+    if(a < b){
+        console.log(a);
+    }else{
+        console.log(b);
+    }
+}
